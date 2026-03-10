6e65f657d911b5c22d5f2b954de00ff41c55912f
hps <hps@live.ru>
2026-03-10 12:37:31 +0300

    Fix naming

---
+++ task/Extensions/JsonElementExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace task.Extensions;
+
+internal static class JsonElementExtensions
+{
+    public static bool TryGetPropertyIgnoreCase(this JsonElement element, string propertyName, out JsonElement jsonElement)
+    {
+        try
+        {
+            jsonElement = element.GetPropertyIgnoreCase(propertyName);
+            return true;
+        }
+        catch (ArgumentNullException)
+        {
+            throw;
+        }
+        catch (Exception)
+        {
+            jsonElement = default;
+            return false;
+        }
+    }
+
+    public static JsonElement GetPropertyIgnoreCase(this JsonElement element, string propertyName)
+    {
+        var jsonElement = element.EnumerateObject().First(o => propertyName.Equals(o.Name, StringComparison.InvariantCultureIgnoreCase));
+        return jsonElement.Value;
+    }
+}
